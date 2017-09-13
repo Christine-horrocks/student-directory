51be30b87bc95b4447312f6d7a6553c986372e49
Christine <splogy2001@hotmail.com>
2017-09-13 21:05:40 +0100

    Challenge 2 complete

--- directory2.rb
+++ directory2.rb
@@ -15,12 +15,6 @@ def input_students
 
 end
 
-def get_letter
-  puts "Type in the letter you wish to search for: "
-  letter = gets.chomp.upcase
-end
-
-
 def print_header
   puts "The students of the Villains Academey"
   puts "-------------"
@@ -28,19 +22,17 @@ end
 
 
 def student_list(names)
-  select_names = names.select { |person| person[0,1] == letter}
-
-  select_names.each.with_index(1) do |name, i|
-    puts " #{i}. #{name[:name]} (#{name[:cohort]} cohort)"
+    names.each do |name|
+    puts " #{name[:name]} (#{name[:cohort]} cohort)" if name [:name].chars.first == "N"
   end
 end
+
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
 students = input_students
-letter = get_letter
 print_header
 student_list(students)
 print_footer(students)
