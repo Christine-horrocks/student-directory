147ed781ca6126e6689d8427e803a6bbbb8883ef
Christine <splogy2001@hotmail.com>
2017-09-13 12:50:53 +0100

    Getting input from users

--- directory.rb
+++ directory.rb
@@ -1,18 +1,31 @@
 
 #Adding an array containg the students
-students = [
-            {name: "Dr. Hannibal Lecter", cohort: :November},
-            {name: "Darth Vader", cohort: :November},
-            {name: "Nurse Ratched", cohort: :November},
-            {name: "Micheal Corleone", cohort: :November},
-            {name: "Alex DeLarge", cohort: :November},
-            {name: "The Wicked Witch of the West", cohort: :November},
-            {name: "Terminator", cohort: :November},
-            {name: "Freddy Krueger", cohort: :November},
-            {name: "The Joker", cohort: :November},
-            {name: "Joffrey Baratheon", cohort: :November},
-            {name: "Norman Bates", cohort: :November}
-]
+#students = [
+          #  {name: "Dr. Hannibal Lecter", cohort: :November},
+          #  {name: "Darth Vader", cohort: :November},
+          #  {name: "Nurse Ratched", cohort: :November},
+          #  {name: "Micheal Corleone", cohort: :November},
+          #  {name: "Alex DeLarge", cohort: :November},
+          #  {name: "The Wicked Witch of the West", cohort: :November},
+          #  {name: "Terminator", cohort: :November},
+          #  {name: "Freddy Krueger", cohort: :November},
+          #  {name: "The Joker", cohort: :November},
+          #  {name: "Joffrey Baratheon", cohort: :November},
+          #  {name: "Norman Bates", cohort: :November}
+#]
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students =[]
+  name = gets.chomp
+    while !name.empty? do
+    students << {name: name, cohort: :November}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+    end
+    students
+end
 
 def print_header
   puts "The students of the Villains Academey"
@@ -29,6 +42,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 student_list(students)
 print_footer(students)
