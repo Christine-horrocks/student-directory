475c00baa944fb05e88b0680126f2ec61d26af3e
Christine <splogy2001@hotmail.com>
2017-09-14 12:54:50 +0100

    Challenge 6 completed

---
+++ directory6.rb
@@ -0,0 +1,38 @@
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students =[]
+  name = gets.chomp
+
+    while !name.empty? do
+    students << {name: name, cohort: :November}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+    end
+
+  students
+
+end
+
+
+def print_header
+  puts "The students of the Villains Academey"
+  puts "-------------".center(37)
+end
+
+
+def student_list(names)
+    names.each.with_index(1) do |name, i|
+    puts " #{i}. #{name[:name]} (#{name[:cohort]} cohort)".center(37)
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students".center(37)
+end
+
+students = input_students
+print_header
+student_list(students)
+print_footer(students)
