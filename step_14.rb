5ace1e6c17e0027a349f35f4fc346922ee7f1dde
Christine <splogy2001@hotmail.com>
2017-09-16 14:52:23 +0100

    Now doesn't need to explicetly close files, but I quite like how it reads when you do expilcitly close them

--- step_14.rb
+++ step_14.rb
@@ -81,24 +81,24 @@ end
 def save_students
   puts "Which file would you like to save this information to? "
   file_name = STDIN.gets.chomp
-  file = File.open(file_name, "w")
+  file = File.open(file_name, "w") do |file|
   @students.each do |student|
       student_data = [student[:name], student[:cohort]]
       csv_line = student_data.join(",")
       file.puts csv_line
     end
-    file.close
+  end
     puts "All student information has been saved"
 end
 
 
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
+  file = File.open(filename, "r") do |file|
   file.readlines.each do |line|
     @name, cohort = line.chomp.split(',')
     push_students
     end
-  file.close
+  end
   puts "Success, #{filename} has been loaded"
 end
 
