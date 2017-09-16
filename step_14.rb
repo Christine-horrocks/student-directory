b2eb411ba92050acca2e7ac44bb8176dd02398f8
Christine <splogy2001@hotmail.com>
2017-09-16 14:08:33 +0100

    Much better nesting

--- step_14.rb
+++ step_14.rb
@@ -1,44 +1,44 @@
 @students = []
 
 def interactive_menu
-loop do
-  print_menu
-  process(STDIN.gets.chomp)
-end
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
 end
 
 
 def print_menu
-puts "1. Input the students"
-puts "2. Show the students"
-puts "3. Save the list to students.csv"
-puts "4. Load the list from students.csv"
-puts "9. Exit"
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
 end
 
 
 def process(selection)
-case selection
-when "1"
-  input_students
-when "2"
-  show_students
-when "3"
-  save_students
-when "4"
-  load_students
-when "9"
-  exit
-else
-  puts " I am not sure what you meant, try again"
-end
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "4"
+      load_students
+    when "9"
+      exit
+    else
+      puts " I am not sure what you meant, try again"
+    end
 end
 
 
 def input_students
-puts "Please enter the names of the students"
-puts "To finish, just hit return twice"
-@name = STDIN.gets.chomp
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  @name = STDIN.gets.chomp
 
     while !@name.empty? do
       push_students
@@ -54,48 +54,48 @@ end
 
 
 def show_students
-print_header
-student_list
-print_footer
+  print_header
+  student_list
+  print_footer
 end
 
 
 def save_students
-file = File.open("students.csv", "w")
-@students.each do |student|
-  student_data = [student[:name], student[:cohort]]
-  csv_line = student_data.join(",")
-  file.puts csv_line
-end
-file.close
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
+    file.close
 end
 
 
 def print_header
-puts "The students of the Villains Academey"
-puts "-------------"
+  puts "The students of the Villains Academey"
+  puts "-------------"
 end
 
 
 def student_list
   @students.each.with_index(1) do |name, i|
   puts " #{i}. #{name[:name]} (#{name[:cohort]} cohort)"
-end
+  end
 end
 
 
 def print_footer
-puts "Overall, we have #{@students.count} great students"
+  puts "Overall, we have #{@students.count} great students"
 end
 
 
 def load_students(filename ="students.csv")
-file = File.open(filename, "r")
-file.readlines.each do |line|
-  @name, cohort = line.chomp.split(',')
-  push_students
-end
-file.close
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    @name, cohort = line.chomp.split(',')
+    push_students
+    end
+  file.close
 end
 
 def try_load_students
